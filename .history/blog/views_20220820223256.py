9665fd4d5cbde0e5b7299321a9961d5b2201d041
christine-gk416 <kelleyc416@gmail.com>
2022-08-20 23:00:36 +0100

    Add more styles to homepage

--- .history/blog/views_20220820223256.py
+++ .history/blog/views_20220820223256.py
@@ -3,7 +3,7 @@ from django.views.generic.list import ListView
 from .models import Post
 
 class PostList(ListView):
-    model = Post
+    model = Post()
     queryset = Post.objects.filter(status=1).order_by('created_on')
-    template_name = 'blog/post-list.html'
+    template_name = 'index.html'
     paginate_by = 6
--- .history/blog/views_20220820223717.py
+++ .history/blog/views_20220820223717.py
@@ -4,6 +4,6 @@ from .models import Post
 
 class PostList(ListView):
     model = Post
-    queryset = Post.objects.filter(status=1).order_by('created_on')
+    # queryset = Post.objects.filter(status=1).order_by('created_on')
     template_name = 'blog/post-list.html'
     paginate_by = 6
